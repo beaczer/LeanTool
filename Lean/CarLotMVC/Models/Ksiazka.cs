4eabfcdb24a50f0095bfb76699360b94b574c1b3
beaczer <czernuszka.beata@gmail.com>
2018-03-21 12:32:17 +0100

    delete

--- Lean/CarLotMVC/Models/Ksiazka.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarLotMVC.Models
-{
-    public class Ksiazka
-    {
-        public int Cena { get; set; }
-        public string Tytul { get; set; }
-        public string Autor { get; set; }
-    }
-}
